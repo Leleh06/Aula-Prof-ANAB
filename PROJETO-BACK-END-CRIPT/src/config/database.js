72024b349703140b43c63c47a81563f480903496
Leleh06 <leticyasilvad1608@gmail.com>
2025-10-06 22:06:07 -0300

    Continuação no projeto back end

--- PROJETO-BACK-END-CRIPT/src/config/database.js
+++ PROJETO-BACK-END-CRIPT/src/config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("senai", "root", "", {
+const sequelize = new Sequelize("senai", "root", "senai", {
     host: "localhost", //aqui você define o host do banco de dados
     dialect: "mysql", //aqui você seleciona o tipo de banco de dados que você está utilizando
 });
